703ce9bebaba285e6e96536a33c68b6f84e9f7b4
ever3622 <ever3622@naver.com>
2021-05-17 12:50:13 +0900

    20210517 중간 커밋

--- home/js/main.js
+++ home/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
     $('.to_top a').smoothScroll();
+    
+    // 모바일용 메뉴 보이기/숨기기 액션처리
+    $('.openMOgnb').click(function(){
+        // alert('메뉴 보이기 액션 클릭'); // 테스트(디버그)
+        // $('.header_cont').css('display','block');
+        $('.header_cont').slideDown('slow');
+        $('#header').addClass("on");
+        // $('.openMOgnb').click(function(){
+        // 	$('.header_cont').slideUp('fast');
+        // 	$('#header').removeClass('on');
+        // });
+    });
+    $('.closePop').click(function(){
+        // $('.header_cont').css('display','none');
+        $('.header_cont').slideUp('fast');
+        $('#header').removeClass('on');
+    });
 });
